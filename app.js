e9516fea8fbaf0a2cbfa8ebb265a2be9c62c68be
AmandaMontana Pearce <amandamontanapearce@gmail.com>
2016-05-31 13:08:19 -0600

    work on ajax get request

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ $('#submit').click('submit', function(event){
 
   $.ajax({
     type:'GET',
-    url:'
-https://api.xmltime.com/astronomy?accesskey=mV4DPzkkK3&expires=2016-05-31T17%3A25%3A40%2B00%3A00&signature=IJM%2BV6ckL9IQ4jtflrjVfnDcTXQ%3D&version=2&object=sun&placeid='+ placeID +'&startdt=2016-05-31',
-    success:
-    sunrise = data.atronomy.objects[1].days[1].events[4].hour + ':' + data.atronomy.objects[1].days[1].events[4].hour
-    sunset = data.atronomy.objects[1].days[1].events[6].hour + ':' + data.atronomy.objects[1].days[1].events[6].hour
-    $('#results').append(<p>The sunrise is</p> + sunrise + <p>The sunset is</p> + sunset);
+    url:'https://api.xmltime.com/astronomy?accesskey=mV4DPzkkK3&expires=201-05-31T17%3A25%3A40%2B00%3A00&signature=IJM%2BV6ckL9IQ4jtflrjVfnDcTXQ%3D&version=2&object=sun&placeid='+ $placeID +'&startdt=2016-05-31',
+    success:(function(data){
+      sunrise = data;
+    // sunrise = data.astronomy.objects[1].days[1].events[4].hour + ':' + data.astronomy.objects[1].days[1].events[4].min;
+    // sunset = data.astronomy.objects[1].days[1].events[6].hour + ':' + data.astronomy.objects[1].days[1].events[6].min;
+    console.log(sunrise);
+    $('#results').append('<p>The sunrise is</p>' + sunrise + '<p>The sunset is</p> + sunset');
+  })//.ajax success
   })// .ajax
 
 })//submit.click
